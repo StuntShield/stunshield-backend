c83cf218ab0ee9a37e97de0266ac7ad5002b20b1
rafaelsety <mhmmdrafael28@gmail.com>
2024-06-01 18:45:15 +0700

    edit main.py file: change port

--- main.py
+++ main.py
@@ -98,4 +98,6 @@ def predict_stunting():
         
 
 if __name__ == "__main__":
-    app.run()
+     # Gunakan PORT dari variabel lingkungan
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
